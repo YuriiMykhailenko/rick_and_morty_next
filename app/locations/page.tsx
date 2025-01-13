7c5529befbabb44448be9570499efb3f4f85acfe
YuriiMykhailenko <urijmihajlenko3@gmail.com>
2025-01-13 13:14:27 +0200

    fix build error for  refresh on mobile

--- app/locations/page.tsx
+++ app/locations/page.tsx
@@ -14,14 +14,14 @@ export default function LocationsPage() {
   return (
     <>
       <main>
-        <WindowRefresher>
-          <Container className="flex flex-col items-center justify-center gap-60 py-40">
-            <Typography variant="h2">Locations List</Typography>
-            <Suspense fallback={<div>Loading...</div>}>
-              <GetLocationsTable />
-            </Suspense>
-          </Container>
-        </WindowRefresher>
+        {/* <WindowRefresher> */}
+        <Container className="flex flex-col items-center justify-center gap-60 py-40">
+          <Typography variant="h2">Locations List</Typography>
+          <Suspense fallback={<div>Loading...</div>}>
+            <GetLocationsTable />
+          </Suspense>
+        </Container>
+        {/* </WindowRefresher> */}
       </main>
     </>
   );
